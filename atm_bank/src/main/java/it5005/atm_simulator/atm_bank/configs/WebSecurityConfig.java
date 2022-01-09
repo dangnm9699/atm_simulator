1a678237177fa6abe915bc03111de196a366800f
dat.nd173011 <dat.nd173011@sis.hust.edu.vn>
2022-01-09 15:02:05 +0700

    Update information

--- atm_bank/src/main/java/it5005/atm_simulator/atm_bank/configs/WebSecurityConfig.java
+++ atm_bank/src/main/java/it5005/atm_simulator/atm_bank/configs/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package it5005.atm_simulator.atm_bank.configs;
 
+import it5005.atm_simulator.atm_bank.jwt.JwtAuthenticationFilter;
+import it5005.atm_simulator.atm_bank.services.CardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,11 +11,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import it5005.atm_simulator.atm_bank.jwt.JwtAuthenticationFilter;
-import it5005.atm_simulator.atm_bank.services.CardService;
 
 
 @EnableWebSecurity
@@ -32,16 +30,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
     @Override
     protected void configure(AuthenticationManagerBuilder auth)
             throws Exception {
         auth.userDetailsService(cardService)
-                .passwordEncoder(passwordEncoder());
+                .passwordEncoder(cardService.passwordEncoder());
     }
 
     @Override
