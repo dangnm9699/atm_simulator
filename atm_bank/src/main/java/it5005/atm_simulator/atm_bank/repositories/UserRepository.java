b20e310239b504c3b45c04251d73fa2524a75e93
dat.nd173011 <dat.nd173011@sis.hust.edu.vn>
2022-01-09 15:04:10 +0700

    Update information

--- atm_bank/src/main/java/it5005/atm_simulator/atm_bank/repositories/UserRepository.java
+++ atm_bank/src/main/java/it5005/atm_simulator/atm_bank/repositories/UserRepository.java
@@ -4,6 +4,13 @@ import it5005.atm_simulator.atm_bank.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByName(String name);
+
+    List<User> findAll();
+
+
 }
