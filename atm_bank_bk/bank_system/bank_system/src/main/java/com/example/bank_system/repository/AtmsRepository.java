d62f61f3c96825facca091c91c000165c548c6b5
dat.nd173011 <dat.nd173011@sis.hust.edu.vn>
2022-01-14 13:35:04 +0700

    Remove file atm_bank_bk/bank_system/bank_system

--- atm_bank_bk/bank_system/bank_system/src/main/java/com/example/bank_system/repository/AtmsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.bank_system.repository;
-
-import com.example.bank_system.model.Atms;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AtmsRepository extends JpaRepository<Atms, Long> {
-
-}
--- atm_bank_bk/bank_system/bank_system/src/main/java/com/example/bank_system/repository/TransactionsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.bank_system.repository;
-
-import com.example.bank_system.model.Transactions;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionsRepository extends JpaRepository<Transactions, Long> {
-}
