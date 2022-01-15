1ba6e45a160397923899b23d050fc320410fa36c
dangnm9699 <dangnm.cs@gmail.com>
2022-01-15 18:03:05 +0700

    update env

--- atm_cash_dispenser/main.py
+++ atm_cash_dispenser/main.py
@@ -26,4 +26,4 @@ app.add_middleware(
 app.include_router(router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("ACD_APP_PORT")))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT")))
--- atm_cash_dispenser/middlewares.py
+++ atm_cash_dispenser/middlewares.py
@@ -5,15 +5,15 @@ from fastapi.security import HTTPBearer
 import jwt
 import time
 
-oauth2 = HTTPBearer(scheme_name=os.getenv("SCHEME_NAME"))
+oauth2 = HTTPBearer(scheme_name=os.getenv("JWT_SCHEME_NAME"))
 
 
 def validate_token(credentials=Depends(oauth2)) -> str:
     try:
         payload = jwt.decode(
             credentials.credentials,
-            os.getenv("SECRET_KEY"),
-            algorithms=[os.getenv("ALGORITHM")],
+            os.getenv("JWT_SECRET_KEY"),
+            algorithms=[os.getenv("JWT_ALGORITHM")],
         )
         if payload.get("exp") < time.time():
             raise HTTPException(status_code=401, detail="Token expired")
