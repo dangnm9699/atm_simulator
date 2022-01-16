7249fef96bb405447d6802b5e91b4efc34bfa8c8
dat.nd173011 <dat.nd173011@sis.hust.edu.vn>
2022-01-16 07:42:30 +0700

    Create payload request and response

---
+++ atm_bank/src/main/java/it5005/atm_simulator/atm_bank/payload/CardResponse.java
@@ -0,0 +1,18 @@
+package it5005.atm_simulator.atm_bank.payload;
+
+import it5005.atm_simulator.atm_bank.models.Card;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class CardResponse {
+    private String number;
+    private String pinHash;
+    private BigDecimal balance;
+
+    public CardResponse(Card card){
+        this.number = card.getNumber();
+        this.balance = card.getBalance();
+    }
+}
