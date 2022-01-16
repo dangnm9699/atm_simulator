43d516c079a253d76bb86f7c4f15257c04d53ed4
dangnm9699 <dangnm.cs@gmail.com>
2022-01-16 22:53:34 +0700

    release 0.2

--- atm_api_gateway/main.go
+++ atm_api_gateway/main.go
@@ -25,7 +25,13 @@ func main() {
 		proxy := httputil.NewSingleHostReverseProxy(target)
 		handler := http.StripPrefix(service.prefix, proxy)
 		nHandler := http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-			log.Println(r.URL.RawPath)
+			rw.Header().Set("Access-Control-Allow-Origin", "*")
+			rw.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+			rw.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+			r.Header.Set("X-Forwarded-Host", r.Header.Get("Host"))
+
+			rw.Header().Set("Access-Control-Allow-Origin", "*")
 			handler.ServeHTTP(rw, r)
 		})
 		http.Handle(service.prefix, nHandler)
