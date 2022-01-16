fa9db7cf9f05e053ef0a55b0b5a188d4076c69a6
GreenBeret3155 <hoangmanhhung99@gmail.com>
2022-01-16 22:08:52 +0700

    [UPDATE] update configure

--- atm_gui/src/assets/env.js
+++ atm_gui/src/assets/env.js
@@ -1,5 +1,8 @@
 (function(window) {
     window["env"] = window["env"] || {};
     // env
-    window["env"]["apiUrl"] = "http://localhost:3001/api/v1";
+    window["env"]["apiUrl"] = "http://localhost:3001";
+    window["env"]["reader"] = "/api/v1";
+    window["env"]["bank"] = "/api/v1";
+    window["env"]["dispenser"] = "/api/v1";
   })(this);
